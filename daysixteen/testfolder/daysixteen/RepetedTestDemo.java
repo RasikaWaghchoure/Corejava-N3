6a424bbfdd4ef7a5d53d0638b39481e7585fca20
Admin <Admin@DESKTOP-IK4J9N0>
2023-02-27 22:29:49 +0530

    Sixteenth Commit

---
+++ daysixteen/testfolder/daysixteen/RepetedTestDemo.java
@@ -0,0 +1,16 @@
+package daysixteen;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+class RepetedTestDemo {
+
+	//@Test
+	@RepeatedTest(5)
+	void test() {
+		System.out.println(" Repeted test");
+	}
+
+}
